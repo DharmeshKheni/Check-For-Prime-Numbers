1601f6cf4cf9b370041dd006525e00e764e77b51
Dharmesh <khenid21@gmail.com>
2015-02-21 13:42:35 +0530

    Check For Prime Number
    Check For Prime Number

---
+++ testing/ViewController.swift
@@ -0,0 +1,93 @@
+//
+//  ViewController.swift
+//  testing
+//
+//  Created by Anil on 21/02/15.
+//  Copyright (c) 2015 Variya Soft Solutions. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    
+    
+    
+    @IBOutlet weak var number: UITextField!
+    
+    
+    
+    @IBOutlet weak var results: UILabel!
+    
+    
+    @IBAction func buttonPressed(sender: AnyObject) {
+        
+        var numberInt = number.text.toInt()
+        
+        if numberInt != nil {
+            
+            var unwrappedNumber = numberInt!
+            
+            
+            var isPrime = true
+            
+            if unwrappedNumber == 1 {
+                
+                isPrime = false
+                
+            }
+            
+            if unwrappedNumber != 2 &&  unwrappedNumber != 1 {
+                
+                for var i = 2; i < unwrappedNumber; i++ {
+                    
+                    
+                    if unwrappedNumber % i == 0 {
+                        isPrime = false
+                        
+                    }
+                }
+                
+            }
+            
+            if isPrime == true{
+                
+                results.text = "\(unwrappedNumber)Is Prime"
+            }else {
+                
+                results.text = "\(unwrappedNumber)Is not Prime"
+            }
+            
+            
+            
+            
+        }else {
+            
+            results.text = "Enter a number"
+        }
+        
+        
+        
+        
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+}
+
